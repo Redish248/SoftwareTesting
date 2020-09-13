01ff501429e6961e22ea2580c2aa6d593f0c00fd
Irina Redkina <irisha132132@mail.ru>
2020-09-13 13:00:28 +0300

    lab1 add tg implementation

--- lab1/src/main/java/task1/FunctionTg.java
+++ lab1/src/main/java/task1/FunctionTg.java
@@ -2,17 +2,87 @@ package task1;
 
 public class FunctionTg {
 
-    private double countBernoulli(int n) {
-        int x = 1;
-
-    }
+    private final double EPS = 1E-10;
 
     public double calculateTg(double x) {
 
+        x = x % Math.PI;
+
+        if (Math.abs(x) + EPS >= Math.PI/2) {
+            throw new IllegalArgumentException();
+        }
+
+        if (x < 0) {
+            return -calculateTg(Math.abs(x));
+        }
+
+        double prevResult = 0;
+        double result = getNextTailor(x,1);
+        double i = 2;
+
+        while (result - prevResult > EPS) {
+            prevResult = result;
+            result += getNextTailor(x, i);
+            i++;
+        }
+
+        return result;
     }
 
-    public double getNextTailor(int n) {
-
+    /**
+     * Count next term in Taylor series
+     * @param x value
+     * @param n number of iteration
+     * @return result
+     */
+    private double getNextTailor(double x, double n) {
+        return countBernoulli(2*n)*Math.pow(-4,n)*(1-Math.pow(4,n))*Math.pow(x,2*n-1)/countFactorial(2*n);
+        //return Math.abs(countBernoulli(2*n)) * Math.pow(2, 2*n) * (Math.pow(2, 2*n) - 1) * Math.pow(x, 2*n - 1) / countFactorial(2*n);
     }
+
+    /**
+     * Count next Bernoulli value
+     * @param n number of iteration
+     * @return result
+     */
+    private double countBernoulli(double n) {
+
+        if (n == 0) return 1;
+        if (n != 1 && n % 2 != 0) return 0;
+
+        double result = countBernoulli(n-1)*countC(2, n+1);
+
+        for (double i = 2; i <= n; i++) {
+            result += countBernoulli(n-i)*countC(i+1, n+1);
+        }
+
+        result *= 1.0/(-n-1);
+
+        return result;
+    }
+
+    /**
+     * Count C(m, n) - Combinations
+     * @param m count of object
+     * @param n all variants
+     * @return result
+     */
+    private double countC(double m, double n) {
+        return countFactorial(n)/(countFactorial(n-m)*countFactorial(m));
+    }
+
+    /**
+     * Count factorial
+     * @param n number
+     * @return result
+     */
+    private double countFactorial(double n) {
+
+        long result = 1;
+        for (double i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    };
 
 }
--- lab1/src/test/java/task1/TgTest.java
+++ lab1/src/test/java/task1/TgTest.java
@@ -1,6 +1,53 @@
 package task1;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TgTest {
+
+    private final double DELTA = 1E-6;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTgPiAndHalf() {
+        assertEquals(Math.tan(Math.PI/2), new FunctionTg().calculateTg(Math.PI/2), DELTA);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTg3PiAndHalf() {
+        assertEquals(Math.tan(3*Math.PI/2), new FunctionTg().calculateTg(3*Math.PI/2), DELTA);
+    }
+
+    @Test
+    public void testTgZero() {
+        assertEquals(Math.tan(0), new FunctionTg().calculateTg(0), DELTA);
+    }
+
+    @Test
+    public void testTgPiAndQuote() {
+        assertEquals(Math.tan(Math.PI/4), new FunctionTg().calculateTg(Math.PI/4), DELTA);
+    }
+
+    @Test
+    public void testTgPiDel3() {
+        assertEquals(Math.tan(Math.PI/3), new FunctionTg().calculateTg(Math.PI/3), DELTA);
+    }
+
+    @Test
+    public void testTgPiDel6() {
+        assertEquals(Math.tan(Math.PI/6), new FunctionTg().calculateTg(Math.PI/6), DELTA);
+    }
+
+    @Test
+    public void testTgMinusPiDel6() {
+        assertEquals(Math.tan(-Math.PI/6), new FunctionTg().calculateTg(-Math.PI/6), DELTA);
+    }
+
+
+    @Test
+    public void testTg2Pi() {
+        assertEquals(Math.tan(2*Math.PI), new FunctionTg().calculateTg(2*Math.PI), DELTA);
+    }
 
 
 }
