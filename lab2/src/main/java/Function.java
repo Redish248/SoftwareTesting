0546cd24aed26ee4edaa06d60c2d8e667fe0c410
olyasoboleva <SobOl99>
2020-10-17 21:09:48 +0300

    Function

--- lab2/src/main/java/Function.java
+++ lab2/src/main/java/Function.java
@@ -1,2 +1,19 @@
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Function {
+    private final Sinus sin;
+    private final Tangens tan;
+    private final LnFunction ln;
+    private final Log5 log5;
+    private final Log2 log2;
+
+    public double calc(double x) {
+        if (x <= 0) {
+            return (((((tan.tg(x) - sin.sin(x)) * Math.pow(tan.tg(x), 3))) - Math.pow(sin.sin(x), 3)));
+        } else {
+            return ((((Math.pow(log5.log5(x), 2)) + log5.log5(x)) - ln.ln(x)) - ((ln.ln(x) / ln.ln(x)) / log2.log2(x)))
+                    / log5.log5(x);
+        }
+    }
 }
