22b95316d0962316f4031773bab9d9cf1de86574
Irina Redkina <irisha132132@mail.ru>
2020-10-17 20:30:05 +0300

    lab2: divide functions

---
+++ lab2/src/main/java/Cosine.java
@@ -0,0 +1,15 @@
+public class Cosine {
+
+    Sinus sinus;
+
+    Cosine() {
+        sinus = new Sinus();
+    }
+
+    public double cos(double x) {
+        x = sinus.checkX(x);
+        double result = Math.sqrt(1 - Math.pow(sinus.sin(x), 2));
+        return (x >= -Math.PI/2 && x <= Math.PI/2) ? result : -result;
+    }
+
+}
