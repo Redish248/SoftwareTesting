6427793c851d3cb309dd533b9e22cfc2ea5ddb16
Irina Redkina <irisha132132@mail.ru>
2020-10-18 16:23:36 +0300

    lab2 log tests lvl1

--- lab2/src/test/java/integration/Log2Test_lvl1.java
+++ lab2/src/test/java/integration/Log2Test_lvl1.java
@@ -1,4 +1,4 @@
-package unit;
+package integration;
 
 import functions.LnFunction;
 import functions.Log2;
@@ -9,45 +9,28 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-public class Log2Test {
 
-    LnFunction lnFunction = mock(LnFunction.class);
+public class Log2Test_lvl1 {
+
+    private final LnFunction lnFunction = new LnFunction();
     private final Log2 log2 = new Log2(lnFunction);
     private final double DELTA = 10E-6;
 
-    {
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(-10);
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(-5);
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(0);
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(-DELTA);
-        when(lnFunction.ln(DELTA)).thenReturn(Math.log(DELTA));
-        when(lnFunction.ln(1)).thenReturn(Math.log(1));
-        when(lnFunction.ln(2)).thenReturn(Math.log(2));
-        when(lnFunction.ln(1+DELTA)).thenReturn(Math.log(1+DELTA));
-        when(lnFunction.ln(1-DELTA)).thenReturn(Math.log(1-DELTA));
-        when(lnFunction.ln(5)).thenReturn(Math.log(5));
-        when(lnFunction.ln(0.5)).thenReturn(Math.log(0.5));
-        when(lnFunction.ln(10)).thenReturn(Math.log(10));
-    }
-
     @ParameterizedTest
-    @ValueSource(doubles = {-5, -10})
+    @ValueSource(doubles = {-DELTA, -5, -10})
     public void testNegative(double x) {
         assertThrows(IllegalArgumentException.class, () -> log2.log2(x));
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {-DELTA, 0})
+    @ValueSource(doubles = {0})
     public void testZero(double x) {
         assertThrows(IllegalArgumentException.class, () -> log2.log2(x));
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {DELTA, 1, 1+DELTA, 1-DELTA})
+    @ValueSource(doubles = {0.1, 1, 1+DELTA, 1-DELTA})
     public void testOne(double x) {
         assertEquals(log2(x), log2.log2(x), DELTA);
     }
--- lab2/src/test/java/integration/Log5Test_lvl1.java
+++ lab2/src/test/java/integration/Log5Test_lvl1.java
@@ -1,41 +1,22 @@
-package unit;
+package integration;
 
 import functions.LnFunction;
 import functions.Log5;
 import org.junit.Ignore;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-public class Log5Test {
-    private final LnFunction lnFunction = mock(LnFunction.class);
+public class Log5Test_lvl1 {
+    private final LnFunction lnFunction = new LnFunction();
     private final Log5 log5 = new Log5(lnFunction);
     private final double DELTA = 10E-6;
 
-    @BeforeEach
-    public void createMock(){
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(-10);
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(-5);
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(0);
-        doThrow(new IllegalArgumentException()).when(lnFunction).ln(-DELTA);
-        when(lnFunction.ln(DELTA)).thenReturn(Math.log(DELTA));
-        when(lnFunction.ln(1)).thenReturn(Math.log(1));
-        when(lnFunction.ln(1+DELTA)).thenReturn(Math.log(1+DELTA));
-        when(lnFunction.ln(1-DELTA)).thenReturn(Math.log(1-DELTA));
-        when(lnFunction.ln(5)).thenReturn(Math.log(5));
-        when(lnFunction.ln(0.5)).thenReturn(Math.log(0.5));
-        when(lnFunction.ln(10)).thenReturn(Math.log(10));
-    }
-
     @ParameterizedTest
-    @ValueSource(doubles = {-5, -10})
+    @ValueSource(doubles = {-1, -5, -10})
     public void testNegative(double x) {
         assertThrows(IllegalArgumentException.class, () -> log5.log5(x));
     }
@@ -47,7 +28,7 @@ public class Log5Test {
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {DELTA, 1, 1+DELTA, 1-DELTA})
+    @ValueSource(doubles = {0.1, 1, 1+DELTA, 1-DELTA})
     public void testOne(double x) {
         assertEquals(log5(x), log5.log5(x), DELTA);
     }
