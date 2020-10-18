a77f5e53bac34f13120d1a8189c74e542111be61
olyasoboleva <SobOl99>
2020-10-18 15:39:59 +0300

    fix pow in Function

--- lab2/src/main/java/functions/Function.java
+++ lab2/src/main/java/functions/Function.java
@@ -12,7 +12,7 @@ public class Function {
 
     public double calc(double x) {
         if (x <= 0) {
-            return (((((tan.calc(x) - sin.calc(x)) * Math.pow(tan.calc(x), 3))) - Math.pow(sin.calc(x), 3)));
+            return Math.pow((Math.pow((tan.calc(x) - sin.calc(x)) * tan.calc(x), 3) - sin.calc(x)), 3);
         } else {
             return ((((Math.pow(log5.log5(x), 2)) + log5.log5(x)) - ln.ln(x)) - ((ln.ln(x) / ln.ln(x)) / log2.log2(x)))
                     / log5.log5(x);
