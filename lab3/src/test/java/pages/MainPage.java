2a0d21a76cb7bd771c48b3e3db8704706f30fa4b
olyasoboleva <SobOl99>
2020-10-31 21:33:29 +0300

    main page - city and guests tests

--- lab3/src/test/java/pages/MainPage.java
+++ lab3/src/test/java/pages/MainPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -15,14 +17,28 @@ public class MainPage extends PageObject {
 
     private final String LOG_IN = "//*[@id='current_account']/a";
     private final String ACCOUNT = "//*[@id='current_account']/a";
-    public final String DESTINATION_INPUT = "//*[@class='sb-destination-label-sr']/input";
-    public final String DESTINATION_SUGGESTIONS = "//*[@data-component='search/destination/input']//span[@class='search_hl_name']";
+    public final String DEST_INPUT = "//*[@class='sb-destination-label-sr']/input";
+    public final String DEST_SUGGESTIONS_NAMES = "//*[@data-component='search/destination/input']//span[@class='search_hl_name']";
+    public final String DEST_SUGGESTIONS_AND_LOADING = "//*[@data-component='search/destination/input']//ul";
     private final String REGISTER_BUTTON = "//*[@id='current_account_create']/a";
     private final String LOG_OUT = "//*[@id=\"profile-menu\"]/form/input[5]";
     private final String ACCOUNT_ICON = "//*[@id=\"current_account\"]/a";
+    private final String GUESTS_LABEL = "//div[@class = 'xp__input-group xp__guests']/label";
+    private final String GUESTS_CONTAINER = "//div[@class = 'xp__input-group xp__guests']/div[contains(@class, 'inputs')]";
 
-    @FindBy(xpath = DESTINATION_INPUT)
+    public final String CITY_LOADING_CLASS = "c-autocomplete__list sb-autocomplete__list sb-autocomplete__list--loading";
+
+    @FindBy(xpath = DEST_INPUT)
     public WebElement destinationInput;
+
+    @FindBy(xpath = DEST_SUGGESTIONS_AND_LOADING)
+    public List<WebElement> cityUlBlocks;
+
+    @FindBy(xpath = GUESTS_LABEL)
+    public WebElement guestsLabel;
+
+    @FindBy(xpath = GUESTS_CONTAINER)
+    public WebElement guestsContainer;
 
     @FindBy(xpath = LOG_IN)
     WebElement logInButton;
@@ -39,8 +55,30 @@ public class MainPage extends PageObject {
     @FindBy(xpath = ACCOUNT_ICON)
     WebElement accountIcon;
 
-    public List<WebElement> getSuggestions() {
-        return driver.findElements(By.xpath(DESTINATION_SUGGESTIONS));
+    public List<WebElement> getSuggestionsNames() {
+        return driver.findElements(By.xpath(DEST_SUGGESTIONS_NAMES));
+    }
+
+    public WebElement getGuestInput(int inputIndex) {
+        return guestsContainer.findElements(By.xpath(".//div[@class='bui-stepper__wrapper sb-group__stepper-a11y']"))
+                .get(inputIndex);
+    }
+
+    public WebElement getGuestButton(WebElement guest, boolean isPlus) {
+        return guest.findElements(By.xpath("./button"))
+                .get(isPlus ? 1 : 0);
+    }
+
+    public WebElement getGuestNumber(WebElement guest) {
+        return guest.findElements(By.xpath("./span")).get(0);
+    }
+
+    public boolean isGuestContainerClosed() {
+        return !this.guestsContainer.getAttribute("style").equals("display: block;");
+    }
+
+    public List<WebElement> getChildrenAgeInput() {
+        return guestsContainer.findElements(By.xpath(".//div[@class='sb-group__children__field clearfix']/select"));
     }
 
     public void focusCity() {
--- lab3/src/test/java/tests/MainPageTest.java
+++ lab3/src/test/java/tests/MainPageTest.java
@@ -1,10 +1,7 @@
 package tests;
 
 import config.WebDriverConfiguration;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.LogInPage;
@@ -16,6 +13,7 @@ import pages.SignUpPage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MainPageTest {
     private static WebDriver webDriver;
     private static MainPage mainPage;
@@ -31,12 +29,14 @@ public class MainPageTest {
     }
 
     @Test
+    @Order(1)
     public void testDestinationEmptyCityNotEmptySuggestions() {
         mainPage.focusCity();
-        assertNotEquals(0, mainPage.getSuggestions().size());
+        assertNotEquals(0, mainPage.getSuggestionsNames().size());
     }
 
     @Test
+    @Order(2)
     public void testDestinationRealCityCheckValue() {
         mainPage.enterCity("Казань");
         assertEquals("Казань", mainPage.destinationInput.getAttribute("value"));
@@ -44,50 +44,220 @@ public class MainPageTest {
     }
 
     @Test
+    @Order(3)
     public void testDestinationRealCityNotEmptySuggestions() {
         mainPage.enterCity("Казань");
         new WebDriverWait(webDriver, 5)
-                .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath(mainPage.DESTINATION_SUGGESTIONS), 0));
-        assertNotEquals(0, mainPage.getSuggestions().size());
+                .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath(mainPage.DEST_SUGGESTIONS_NAMES), 0));
+        assertNotEquals(0, mainPage.getSuggestionsNames().size());
     }
 
     @Test
+    @Order(4)
     public void testDestinationRealCityFirstSuggestion() {
         mainPage.enterCity("Казань");
         new WebDriverWait(webDriver, 5)
-                .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath(mainPage.DESTINATION_SUGGESTIONS), 0));
-        WebElement firstSuggestion = mainPage.getSuggestions().get(0);
+                .until(ExpectedConditions.attributeContains(mainPage.cityUlBlocks.get(0),
+                        "class", "-visible"));
+        WebElement firstSuggestion = mainPage.getSuggestionsNames().get(0);
         assertEquals("Казань", firstSuggestion.getText());
     }
 
     @Test
+    @Order(5)
     public void testDestinationRealCityWithMistake() {
         mainPage.enterCity("Казаньб");
-        assertNotEquals(0, mainPage.getSuggestions().size());
+        new WebDriverWait(webDriver, 5)
+                .until(ExpectedConditions.attributeContains(mainPage.cityUlBlocks.get(0),
+                        "class", "-visible"));
+        WebElement firstSuggestion = mainPage.getSuggestionsNames().get(0);
+        assertEquals("Казань", firstSuggestion.getText());
 
     }
+
+    @Test
+    @Order(6)
+    public void testDestinationFakeCity() {
+        mainPage.enterCity("Йцукен");
+        new WebDriverWait(webDriver, 5)
+                .until(ExpectedConditions.attributeToBe(mainPage.cityUlBlocks.get(1),
+                        "class", mainPage.CITY_LOADING_CLASS));
+        assertFalse(mainPage.cityUlBlocks.get(0).getAttribute("class").contains("-visible"));
+    }
+
+    @Test
+    @Order(7)
+    @Disabled("В целом работает, но оооооочень долго")
+    public void testDestinationLongNameCity() {
+        mainPage.enterCity("НуОченьДлинноеНазваниеГородаДляТестаВводаНаБукинге".repeat(276));
+        new WebDriverWait(webDriver, 5)
+                .until(ExpectedConditions.attributeToBe(mainPage.cityUlBlocks.get(1),
+                        "class", mainPage.CITY_LOADING_CLASS));
+        assertFalse(mainPage.cityUlBlocks.get(0).getAttribute("class").contains("-visible"));
+    }
+
+    @Test
+    @Order(8)
+    public void testGuestsDisplayContainerAfterClick() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        assertEquals("display: block;", mainPage.guestsContainer.getAttribute("style"));
+    }
+
+    @Test
+    @Order(9)
+    public void testGuestsAdultMinusButton() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        String expValue = String.valueOf(Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(0)).getText())-1);
+        mainPage.getGuestButton(mainPage.getGuestInput(0), false).click();
+        assertEquals(expValue, mainPage.getGuestNumber(mainPage.getGuestInput(0)).getText());
+    }
+
+    @Test
+    @Order(10)
+    public void testGuestsAdultMinusButtonDisabled() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        while (!"1".equals(mainPage.getGuestNumber(mainPage.getGuestInput(0)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(0), false).click();
+        }
+        assertTrue(mainPage.getGuestButton(mainPage.getGuestInput(0), false).getAttribute("class").contains("disabled"));
+    }
+
+    @Test
+    @Order(11)
+    public void testGuestsAdultPlusButton() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        String expValue = String.valueOf(Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(0)).getText())+1);
+        mainPage.getGuestButton(mainPage.getGuestInput(0), true).click();
+        assertEquals(expValue, mainPage.getGuestNumber(mainPage.getGuestInput(0)).getText());
+    }
+
+    @Test
+    @Order(12)
+    public void testGuestsAdultPlusButtonDisabled() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        while (!"30".equals(mainPage.getGuestNumber(mainPage.getGuestInput(0)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(0), true).click();
+        }
+        assertTrue(mainPage.getGuestButton(mainPage.getGuestInput(0), true).getAttribute("class").contains("disabled"));
+    }
+
+    @Test
+    @Order(13)
+    public void testGuestsChildPlusButton() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        String expValue = String.valueOf(Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText())+1);
+        mainPage.getGuestButton(mainPage.getGuestInput(1), true).click();
+        assertEquals(expValue, mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText());
+    }
+
+    @Test
+    @Order(14)
+    public void testGuestsChildPlusButtonDisabled() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        while (!"10".equals(mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(1), true).click();
+        }
+        assertTrue(mainPage.getGuestButton(mainPage.getGuestInput(1), true).getAttribute("class").contains("disabled"));
+    }
+
+    @Test
+    @Order(15)
+    public void testGuestsChildMinusButton() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        String expValue = String.valueOf(Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText())-1);
+        mainPage.getGuestButton(mainPage.getGuestInput(1), false).click();
+        assertEquals(expValue, mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText());
+    }
+
+    @Test
+    @Order(16)
+    public void testGuestsChildMinusButtonDisabled() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        while (!"0".equals(mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(1), false).click();
+        }
+        assertTrue(mainPage.getGuestButton(mainPage.getGuestInput(1), false).getAttribute("class").contains("disabled"));
+    }
+
+    @Test
+    @Order(17)
+    public void testGuestsRoomPlusButton() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        String expValue = String.valueOf(Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(2)).getText())+1);
+        mainPage.getGuestButton(mainPage.getGuestInput(2), true).click();
+        assertEquals(expValue, mainPage.getGuestNumber(mainPage.getGuestInput(2)).getText());
+    }
+
+    @Test
+    @Order(18)
+    public void testGuestsRoomMinusButton() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        String expValue = String.valueOf(Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(2)).getText())-1);
+        mainPage.getGuestButton(mainPage.getGuestInput(2), false).click();
+        assertEquals(expValue, mainPage.getGuestNumber(mainPage.getGuestInput(2)).getText());
+    }
+
+    @Test
+    @Order(19)
+    public void testGuestsRoomMinusButtonDisabled() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        while (!"1".equals(mainPage.getGuestNumber(mainPage.getGuestInput(2)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(2), false).click();
+        }
+        assertTrue(mainPage.getGuestButton(mainPage.getGuestInput(2), false).getAttribute("class").contains("disabled"));
+    }
+
+    @Test
+    @Order(20)
+    public void testGuestsRoomPlusButtonDisabled() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        while (!"30".equals(mainPage.getGuestNumber(mainPage.getGuestInput(2)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(2), true).click();
+        }
+        assertTrue(mainPage.getGuestButton(mainPage.getGuestInput(2), true).getAttribute("class").contains("disabled"));
+    }
+
+    @Test
+    @Order(21)
+    public void testChildrenAgeInputsAmount() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        int expResult = Integer.parseInt(mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText())+3;
+        mainPage.getGuestButton(mainPage.getGuestInput(1), true).click();
+        mainPage.getGuestButton(mainPage.getGuestInput(1), true).click();
+        mainPage.getGuestButton(mainPage.getGuestInput(1), true).click();
+        assertEquals(expResult, mainPage.getChildrenAgeInput().size());
+    }
+
+    @Test
+    @Order(22)
+    public void testChildrenAgeInput() {
+        if (mainPage.isGuestContainerClosed()) mainPage.guestsLabel.click();
+        if ("0".equals(mainPage.getGuestNumber(mainPage.getGuestInput(1)).getText())) {
+            mainPage.getGuestButton(mainPage.getGuestInput(1), true).click();
+        }
+        WebElement select = mainPage.getChildrenAgeInput().get(0);
+        WebElement option = select.findElement(By.xpath("./option[@value='2']"));
+        option.click();
+        assertTrue(option.isSelected());
+    }
+
 
     @Test
     @Disabled
-    public void testDestinationFakeCity() {
-        mainPage.enterCity("Йцукен");
-        assertEquals(0, mainPage.getSuggestions().size());
-
-    }
-
-    @Test
     public void testGoToLogInPage() {
         mainPage.clickToLogIn();
         assertTrue(logInPage.isEmailInputDisplayed());
     }
 
     @Test
+    @Disabled
     public void testGoToSignInPage() {
         mainPage.goToRegisterPage();
         assertTrue(signUpPage.isEmailInputDisplayed());
     }
 
     @Test
+    @Disabled
     public void testLogOut() {
         mainPage.clickToLogIn();
         logInPage.signIn("yecine5178@aieen.com", "12345678");
